66ab522bd96b1b697bb95cce2aadf1a838c77873
[machiren] <[machiren111@gmail.com]>
2020-02-24 21:45:56 +0900

    #1 TypeORM構築

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'password',
+    database: 'todolist',
+    entities: [],
+    synchronize: true,
+  }),
+    // UsersModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    // UserService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
